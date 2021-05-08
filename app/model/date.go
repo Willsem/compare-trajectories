93f611076dcb8e6f39f7da0551e209430987a041
Willsem <happymainer@gmail.com>
2021-05-08 15:28:29 +0300

    refactor unmarshal

--- app/model/date.go
+++ app/model/date.go
@@ -9,14 +9,14 @@ type ParsedDate struct {
 	time.Time
 }
 
-func (c *ParsedDate) UnmarshalJSON(b []byte) (err error) {
+func (d *ParsedDate) UnmarshalJSON(b []byte) (err error) {
 	const layout = "01/02/06 15:04:05.000000"
 
-	s := strings.Trim(string(b), "\"") // remove quotes
+	s := strings.Trim(string(b), "\"")
 	if s == "null" {
 		return
 	}
 
-	c.Time, err = time.Parse(layout, s)
+	d.Time, err = time.Parse(layout, s)
 	return
 }
