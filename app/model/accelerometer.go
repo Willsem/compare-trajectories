1d163f968ef3566bfaf349150dda360bd3f5fee9
Willsem <happymainer@gmail.com>
2021-05-08 13:51:11 +0300

    change model format to array

--- app/model/accelerometer.go
+++ app/model/accelerometer.go
@@ -3,14 +3,7 @@ package model
 import "time"
 
 type Acceletometer struct {
-	Date time.Time `json:"date"`
-	Acc  Point     `json:"acc"`
-	Gyro Point     `json:"gyro"`
-}
-
-func NewAccelerometer(date string, a Point, g Point) (acc *Acceletometer, err error) {
-	acc.Date, err = parseDate(date)
-	acc.Acc = a
-	acc.Gyro = g
-	return
+	Date []time.Time `json:"date"`
+	Acc  []Point     `json:"acc"`
+	Gyro []Point     `json:"gyro"`
 }
--- app/model/gps.go
+++ app/model/gps.go
@@ -3,14 +3,7 @@ package model
 import "time"
 
 type Gps struct {
-	Date time.Time `json:"date"`
-	Long float32   `json:"long"`
-	Lat  float32   `json:"lat"`
-}
-
-func NewGps(date string, long, lat float32) (gps *Gps, err error) {
-	gps.Date, err = parseDate(date)
-	gps.Long = long
-	gps.Lat = lat
-	return
+	Date []time.Time `json:"date"`
+	Long []float32   `json:"long"`
+	Lat  []float32   `json:"lat"`
 }
