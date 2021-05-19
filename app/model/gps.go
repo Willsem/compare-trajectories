faf250dea3032ff1cb4e0990594ae546b391fcb3
Willsem <happymainer@gmail.com>
2021-05-20 02:43:13 +0300

    add methods for gps model

--- app/model/gps.go
+++ app/model/gps.go
@@ -1,8 +1,21 @@
 // Package model ...
 package model
 
+import "errors"
+
 type Gps struct {
 	Date []ParsedDate `json:"date"`
 	Long []float64    `json:"long"`
 	Lat  []float64    `json:"lat"`
+}
+
+func (gps *Gps) Check() error {
+	if len(gps.Date) != len(gps.Long) || len(gps.Date) != len(gps.Lat) || len(gps.Long) != len(gps.Lat) {
+		return errors.New("various len of arrays in gps")
+	}
+	return nil
+}
+
+func (gps *Gps) Len() int {
+	return len(gps.Long)
 }
