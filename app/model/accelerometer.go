713c99f419d5f0e83e9946c189e5d0d67be4c494
Willsem <happymainer@gmail.com>
2021-05-20 02:46:19 +0300

    add methods for accelerometer model

--- app/model/accelerometer.go
+++ app/model/accelerometer.go
@@ -1,8 +1,21 @@
 // Package model ...
 package model
 
-type Acceletometer struct {
+import "errors"
+
+type Accelerometer struct {
 	Date []ParsedDate `json:"date"`
 	Acc  []Point      `json:"acc"`
 	Gyro []Point      `json:"gyro"`
+}
+
+func (acc *Accelerometer) Check() error {
+	if len(acc.Date) != len(acc.Acc) || len(acc.Date) != len(acc.Gyro) || len(acc.Acc) != len(acc.Gyro) {
+		return errors.New("various len of arrays in gps")
+	}
+	return nil
+}
+
+func (acc *Accelerometer) Len() int {
+	return len(acc.Acc)
 }
