abb0a0336a50f5b4f3ccb75429680e1d98836f57
Willsem <happymainer@gmail.com>
2021-05-19 18:06:43 +0300

    add comparing service

---
+++ app/model/compared_trajectory.go
@@ -0,0 +1,8 @@
+// Package model ...
+package model
+
+type ComparedTrajectory struct {
+	Backlog []float64
+	Long    []float64
+	Lat     []float64
+}
--- app/service/comparing/compare.go
+++ app/service/comparing/compare.go
@@ -3,5 +3,6 @@ package comparing
 
 import "github.com/Willsem/compare-trajectories/app/model"
 
-func Compare(perfect model.Trajectory, compared model.Trajectory) {
+func Compare(perfect model.Trajectory, compared model.Trajectory) model.ComparedTrajectory {
+	return model.ComparedTrajectory{}
 }
