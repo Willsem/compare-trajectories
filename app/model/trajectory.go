2b11e2bcef0a9edd1510de4c21d19e0dda263327
Willsem <happymainer@gmail.com>
2021-05-20 03:04:36 +0300

    add check method for trajectory

--- app/model/trajectory.go
+++ app/model/trajectory.go
@@ -5,3 +5,15 @@ type Trajectory struct {
 	Gps           Gps           `json:"gps"`
 	Accelerometer Accelerometer `json:"acc"`
 }
+
+func (t *Trajectory) Check() error {
+	if err := t.Gps.Check(); err != nil {
+		return err
+	}
+
+	if err := t.Accelerometer.Check(); err != nil {
+		return err
+	}
+
+	return nil
+}
