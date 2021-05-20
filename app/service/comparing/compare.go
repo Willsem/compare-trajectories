a9572f167159a20f69d13db285219e611e4e56d1
Willsem <happymainer@gmail.com>
2021-05-20 03:09:27 +0300

    add check

--- app/service/comparing/compare.go
+++ app/service/comparing/compare.go
@@ -1,8 +1,24 @@
 // Package comparing ...
 package comparing
 
-import "github.com/Willsem/compare-trajectories/app/model"
+import (
+	"github.com/Willsem/compare-trajectories/app/model"
+)
 
-func Compare(perfect model.Trajectory, compared model.Trajectory) (model.ComparedTrajectory, error) {
-	return model.ComparedTrajectory{}, nil
+type speedTrajectory struct {
+	Speed []float64
+}
+
+func Compare(perfect model.Trajectory, compared model.Trajectory) (ct model.ComparedTrajectory, err error) {
+	err = perfect.Check()
+	if err != nil {
+		return
+	}
+
+	err = compared.Check()
+	if err != nil {
+		return
+	}
+
+	return
 }
