521d521925f0f522ea9d8c2d22ede59b0f1c1ce1
Willsem <happymainer@gmail.com>
2021-05-22 16:15:27 +0300

    make changedetection package

--- app/service/comparing/changedetection/change_point_detection.go
+++ app/service/comparing/changedetection/change_point_detection.go
@@ -1,8 +1,34 @@
 // Package changedetection ...
 package changedetection
 
-import "github.com/Willsem/compare-trajectories/app/model"
+import (
+	"github.com/Willsem/compare-trajectories/app/model"
+	"pgregory.net/changepoint"
+)
 
 func ChangePointDetection(trajectory *model.ComparedTrajectory) []model.ComparedTrajectory {
-	return nil
+	data := trajectory.Backlog
+
+	indexes := changepoint.NonParametric(data, 1)
+	indexes = append([]int{0}, indexes...)
+	indexes = append(indexes, len(trajectory.Backlog)-1)
+
+	result := make([]model.ComparedTrajectory, 0)
+	for i := 0; i < len(indexes)-1; i++ {
+		start := indexes[i]
+		finish := indexes[i+1]
+
+		part := model.ComparedTrajectory{}
+		for j := start; j <= finish; j++ {
+			part.Backlog = append(part.Backlog, trajectory.Backlog[j])
+			part.DeltaLat = append(part.Backlog, trajectory.DeltaLat[j])
+			part.DeltaLong = append(part.Backlog, trajectory.DeltaLong[j])
+			part.DeltaAcc = append(part.DeltaAcc, trajectory.DeltaAcc[j])
+			part.DeltaGyro = append(part.DeltaAcc, trajectory.DeltaGyro[j])
+		}
+
+		result = append(result, part)
+	}
+
+	return result
 }
