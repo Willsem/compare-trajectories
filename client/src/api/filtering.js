bc6cc40aeefe5977b5624bc9879102a31c2e4fcc
Willsem <happymainer@gmail.com>
2021-05-25 18:37:27 +0300

    add client config

--- client/src/api/filtering.js
+++ client/src/api/filtering.js
@@ -1,17 +1,18 @@
 import $ from 'jquery';
+import config from '../configs/client.json';
 
 export function filtering(trajectory) {
-  let filtered_trajectory;
+  let filteredTrajectory;
 
   $.ajax({
-    url: 'http://localhost:8080/filter',
+    url: config.server + '/filter',
     method: 'post',
     async: false,
     data: JSON.stringify(trajectory),
   })
   .done(function(result) {
-    filtered_trajectory = result;
+    filteredTrajectory = result;
   });
 
-  return filtered_trajectory;
+  return filteredTrajectory;
 }
