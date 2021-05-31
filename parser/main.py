e940f08738ae7d1f954a02cc61b9cb5061e3c2c9
Willsem <happymainer@gmail.com>
2021-05-31 03:42:33 +0300

    add settings for interval of result trajectory

--- parser/main.py
+++ parser/main.py
@@ -9,13 +9,15 @@ def create_argument_parser():
     parser.add_argument('-a', '--acc', required=True)
     parser.add_argument('-g', '--gps', required=True)
     parser.add_argument('-o', '--out', required=True)
+    parser.add_argument('-s', '--start', required=False, type=int)
+    parser.add_argument('-f', '--finish', required=False, type=int)
     return parser
 
 
-def parse_files(gps_filename, acc_filename, out):
+def parse_files(gps_filename, acc_filename, out, start, finish):
     with open(gps_filename) as gpsfile:
         with open(acc_filename) as accfile:
-            data = parse(gpsfile, accfile)
+            data = parse(gpsfile, accfile, start, finish)
 
     with open(out, 'w') as file:
         file.write(json.dumps(data))
@@ -25,7 +27,7 @@ def main():
     parser = create_argument_parser()
     args = parser.parse_args(sys.argv[1:])
 
-    parse_files(args.gps, args.acc, args.out)
+    parse_files(args.gps, args.acc, args.out, args.start, args.finish)
     print(f'Create file {args.out}')
 
 
--- parser/parser.py
+++ parser/parser.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Point:
     def __init__(self, x, y, z):
         self.x = x
@@ -77,8 +80,53 @@ def parse_gps(file):
     return parsed_data.__dict__
 
 
-def parse(gpsfile, accfile):
-    return get_dict(
-        parse_gps(gpsfile),
-        parse_acc(accfile),
-    )
+def cut(gps, acc, start, finish):
+    if start is None and finish is None:
+        return gps, acc
+    if start is None:
+        start = 0
+    if finish is None:
+        finish = len(gps['date'])
+    gps = {
+        'date': gps['date'][start:finish],
+        'long': gps['long'][start:finish],
+        'lat':  gps['lat'][start:finish],
+    }
+
+    def convert_date(string):
+        return datetime.strptime(string, '%m/%d/%y %H:%M:%S.%f')
+
+    begin = convert_date(gps['date'][0])
+    end = convert_date(gps['date'][-1])
+
+    min_beg = -1
+    min_end = -1
+    min_beg_i = -1
+    min_end_i = -1
+
+    for i in range(len(acc['date'])):
+        dist_b = abs(convert_date(acc['date'][i]) - begin).microseconds
+        dist_e = abs(convert_date(acc['date'][i]) - end).microseconds
+
+        if min_beg == -1 or dist_b < min_beg:
+            min_beg = dist_b
+            min_beg_i = i
+
+        if min_end == -1 or dist_e < min_end:
+            min_end = dist_e
+            min_end_i = i
+
+    acc = {
+        'date': acc['date'][min_beg_i:min_end_i],
+        'gyro': acc['gyro'][min_beg_i:min_end_i],
+        'acc': acc['acc'][min_beg_i:min_end_i],
+    }
+
+    return gps, acc
+
+
+def parse(gpsfile, accfile, start, finish):
+    gps = parse_gps(gpsfile)
+    acc = parse_acc(accfile)
+    gps, acc = cut(gps, acc, start, finish)
+    return get_dict(gps, acc)
